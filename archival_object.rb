e6f8f5f68988e75622dc2f1d529ab81cbffbb329
Mark A. Matienzo <mark@matienzo.org>
2012-05-15 09:44:35 -0400

    More appropriately reflects Rails' object lifecycle

--- archival_object.rb
+++ archival_object.rb
@@ -3,24 +3,31 @@ require 'state_machine'
 class ArchivalObject
   
   state_machine :initial => :new do
-    event :validate do
-      transition all => :valid, :if => :valid?
-      transition all => :invalid
+    event :update do
+      transition all - [:deleted, :deletion_requested] => :updated, :if => :valid?
     end
     
     event :suppress do
-      transition all - [:new, :invalid, :suppressed] => :suppressed
+      transition all - [:suppressed, :deleted] => :suppressed
     end
     
     event :unsuppress do
-      transition :suppressed => :valid, :if => :valid?
-      transition :suppressed => :invalid
+      transition :suppressed => :updated
     end
     
     event :request_deletion do
-      transition all - [:new, :deletion_requested] => :deletion_requested
+      transition all - [:new, :deleted, :deletion_requested] => :deletion_requested
     end
-  
+    
+    event :cancel_request do
+      transition :deletion_requested => :updated
+    end
+    
+    event :destroy do
+      transition all - [:deleted] => :deleted, :if => :user_is_admin?
+      transition :deletion_requested => :deleted, :if => :user_can_delete?
+    end
+    
     state all - [:deletion_requested] do
       def deletable?
         false
@@ -34,8 +41,4 @@ class ArchivalObject
     end
     
   end
-
-  def valid?
-  end
-  
 end
